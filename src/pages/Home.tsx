0174fb57ed841b94d3adac09c1e5ed0eaca11eaa
Naiara Medeiros <naiaramc.almeida@gmail.com>
2023-05-06 15:46:58 -0300

    Caixa Login

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,12 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import CaixaLogin from '../components/CaixaLogin';
 import './Home.css';
 
 const Home: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+        <CaixaLogin />
       </IonContent>
     </IonPage>
   );
